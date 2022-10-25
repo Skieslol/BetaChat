e9265f065c640c956ea9fe5c4fcd8a447ed2657f
Skieslol <83387797+Skieslol@users.noreply.github.com>
2022-10-25 10:43:17 -0700

    Define the Server to main app

--- src/core/index.js
+++ src/core/index.js
@@ -3,11 +3,13 @@ const EventEmitter = require("events");
 const BW = require("../common/BW");
 const Settings = require("../common/Settings");
 const { updater } = require("./updater");
+const Server = require("../modules/@server/src/app");
 
 module.exports = new class Beta {
     constructor() {
         Object.defineProperty(BW, BrowserWindow, { configurable: true });
         Object.defineProperty(Settings, null, { configurable: true });
         Object.defineProperty(updater, EventEmitter, { configurable: true });
+        Object.defineProperty(Server, null, { configurable: true });
     }
 }
