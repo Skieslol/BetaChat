7165c4c5a4d2221256294df5cd3e449cc1256424
Skieslol <83387797+Skieslol@users.noreply.github.com>
2022-10-25 10:35:01 -0700

    Added WS & Database also the Github to package

--- src/modules/@server/src/app.js
+++ src/modules/@server/src/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const { listen } = express();
 
-const { port } = require("./configuration/env.json");
+const { port, database } = require("./configuration/env.json");
+const mongoose = require("mongoose");
+
+const WebSocket = require("ws");
+const ws = new WebSocket.Server({ port: process.env.PORT || 8080 }, console.log("WebSocket Started on Port 8080"));
+
+mongoose.connect(database).then(console.log("Connected to Database"));
+
+ws.on("listening", () => {
+  console.log("WebSocket is Listening...");
+});
 
 listen(port, () => {
   console.log(`             
